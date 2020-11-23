7236cc24d477a37722e60b2fe6cc4e3681d8d401
evombau <eikevombauer@live.com>
2020-11-23 19:02:38 +0100

    updated to material-calendar 2.0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarConfig } from 'material-calendar';
+import { CalendarConfig, DayC } from 'material-calendar';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -10,48 +10,48 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class AppComponent implements OnInit {
   title = 'Material Calendar Demo';
   durationInSeconds = 5;
+  isLoading = true
 
   placeholder = false // boolean
-  mode = 'monthly' // 'annual' | 'monthly'
   singleOrMulti = 'multi'
 
   monthsAfterBefore = Array(11).fill(0).map((x, i) => i);
   monthsBefore = 0;
   monthsAfter = 0;
 
+  
   calendarConfig: CalendarConfig = {
-    panelBgColor: '#00677f', // 00677f 006105
-    autoTextColor: true,
-    textColor: '#fff',
-    useHolidays: false,
-    holidayColor: 'rgb(253, 173, 0)',
-    holidayTextColor: 'rgb(253, 173, 0)',
-    displayYear: true,
+    renderMode: 'monthly', // 'annual' | 'monthly'
+    selectMode: 'range',  // 'click' | 'range'
+    displayYear: false,
     firstDayOfWeekMonday: true,
     calendarWeek: true,
-    todayColor: '#fff',
-    panelWidth: '90%',
     switches: true,
+    panelWidth: '30%'
   }
+
+  dataSource: DayC[] = [
+    {
+      date: 1604185200000,
+      backgroundColor: 'rgba(0, 0, 255, 0.5)',
+      toolTip: 'Test ToolTip',
+      badgeMode: 'Icon',
+      badgeInt: 5,
+      badgeIcon: 'edit'
+    },
+    {
+      date: 1604199900000,
+      backgroundColor: 'blue'
+    }
+  ]
 
   constructor(private _snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.isLoading = false
   }
 
-  switchMode() {
-    this.mode = (this.mode === 'monthly') ? 'annual' : 'monthly'
-  }
-
-  openSnackBar(event) { // JSON.stringify(event) `kw: ${event.kw}, `
-    this._snackBar.open(`kw: ${event.kw},
-    day: ${event.day},
-    date: ${event.date},
-    isWeekendDay: ${event.isWeekendDay},
-    isHoliday: ${event.isHoliday},
-    type: ${event.type}`, '', {
-      duration: this.durationInSeconds * 1000,
-      panelClass: ['success-snackbar']
-    });
+  testMethod(event) {
+    console.log(event)
   }
 }
