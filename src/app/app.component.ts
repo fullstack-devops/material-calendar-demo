789f066deccf27a488f82e963b298e3c62bee2ed
eksrha <58111764+eksrha@users.noreply.github.com>
2021-08-22 14:56:40 +0200

    update to material-calendar@3.1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { CalendarConfig, DayC } from 'material-calendar';
 
 @Component({
@@ -9,61 +11,64 @@ import { CalendarConfig, DayC } from 'material-calendar';
 export class AppComponent implements OnInit {
   title = 'Material Calendar Demo';
 
+  // Theming
+  toggleControl = new FormControl(false);
+  @HostBinding('class') className = '';
+
   placeholder = false // boolean
   isLoading = true
   latestEvent = ""
-  month=5
-  year=2021
 
   monthsAfterBefore = Array(5).fill(0).map((x, i) => i);
-  monthsBefore = 1;
-  monthsAfter = 1;
+  monthsBefore = 0;
+  monthsAfter = 2;
 
   calendarConfig: CalendarConfig = {
     renderMode: 'monthly', // 'annual' | 'monthly'
-    selectMode: 'click',  // 'click' | 'range'
+    selectMode: 'range',  // 'click' | 'range'
     displayYear: true,
     firstDayOfWeekMonday: true,
     calendarWeek: true,
     switches: true,
-    panelWidth: '300px'
+    panelWidth: '300px',
+    bluredDays: false, // default: false
+    markWeekend: true // default: true
   }
 
   dataSource: DayC[] = [
     {
-      date: 1624312800000,
+      date: 1634594400000,
       backgroundColor: '#0167c7',
-      toolTip: 'Test ToolTip',
-      badgeMode: 'Icon',
-      badgeInt: 5,
-      badgeIcon: 'edit'
+      toolTip: 'Test ToolTip First',
     },
     {
-      date: 1624312800000,
+      date: 1634594400000,
       backgroundColor: 'rgb(6, 182, 0)',
-      toolTip: 'Test ToolTip',
-      badgeMode: 'Icon',
-      badgeInt: 5,
-      badgeIcon: 'edit'
+      toolTip: 'Test ToolTip Second',
     },
     {
-      date: 1624658400000,
+      date: 1634853600000,
       backgroundColor: 'rgb(6, 182, 0)',
       toolTip: 'Test ToolTip 2',
-      /* badgeMode: 'Icon',
-      badgeInt: 5,
-      badgeIcon: 'edit' */
     },
     {
-      date: 1604199900000,
-      backgroundColor: 'blue'
+      date: 1635544800000,
+      backgroundColor: 'lightblue'
     }
   ]
 
-  constructor() {
-  }
+  constructor(private overlay: OverlayContainer) { }
+  ngOnInit(): void {
+    this.toggleControl.valueChanges.subscribe((darkMode) => {
+      const darkClassName = 'darkMode';
+      this.className = darkMode ? darkClassName : '';
+      if (darkMode) {
+        this.overlay.getContainerElement().classList.add(darkClassName);
+      } else {
+        this.overlay.getContainerElement().classList.remove(darkClassName);
+      }
+    });
 
-  ngOnInit() {
     console.log(this.dataSource)
     this.isLoading = false
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { MaterialCalendarModule } from 'material-calendar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -19,12 +22,17 @@ import { MatSelectModule } from '@angular/material/select';
     BrowserAnimationsModule,
     FormsModule,
     MatSlideToggleModule,
+    MatButtonModule,
     MatFormFieldModule,
+    ReactiveFormsModule,
     MatSelectModule,
+    MatIconModule,
+    MatSelectModule,
+    MatToolbarModule,
     MaterialCalendarModule
   ],
   providers: [
-    {provide: LOCALE_ID, useValue: 'de-DE' }
+    { provide: LOCALE_ID, useValue: 'de-DE' }
   ],
   bootstrap: [AppComponent]
 })
